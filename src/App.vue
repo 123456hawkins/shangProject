e6290bef4e63a35526a25307a40553ac89c31578
123456hawkins <2398430768@qq.com>
2024-02-05 09:55:16 +0800

    [feat]新增prettier

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,10 @@
   <div>我的vite+vue项目</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+var a = ref('1')
+console.log(a)
+</script>
 <style scoped lang="scss"></style>
