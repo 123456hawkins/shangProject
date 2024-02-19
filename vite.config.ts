682e64bd36bc1afb51e671372a3ace4842a22fb9
123456hawkins <2398430768@qq.com>
2024-02-19 10:45:06 +0800

    feat: 新增elementPlus和配置@地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), //路径相对别名，用@代替src
+    },
+  },
 })
