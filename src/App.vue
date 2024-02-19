b97e28c9aabea1c0c5f9352a84d5ddc9af7cb0ba
123456hawkins <2398430768@qq.com>
2024-02-19 17:28:51 +0800

    feat: 新增axios二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <div>
-    <span class="container">ceshi</span>
-    <svg-icon name="phone" color="red"></svg-icon>
-    <svg-icon name="back" color="green"></svg-icon>
-  </div>
+  <div></div>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios'
-// 测试假接口
-axios({
-  url: '/api/user/login',
-  method: 'post',
-  data: {
-    username: 'admin',
-    password: '11111',
-  },
-}).then((res) => {
-  console.log(res)
+import { onMounted } from 'vue'
+import request from '@/utils/request'
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
 })
 </script>
-<style scoped lang="scss">
-.container {
-  color: $color;
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,51 @@
+// 进行axios二次封装
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+// 利用axios对象的create方法，去创建axios实例(基础路径，超时时间)
+const request = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 5000, //超时时间
+})
+// 请求拦截器
+request.interceptors.request.use((config) => {
+  // config配置对象，headers属性请求头
+  // 返回配置对象
+  // 不return就发不出请求
+  return config
+})
+// 响应拦截
+request.interceptors.response.use(
+  (response) => {
+    // 成功回调
+    // 简化数据
+    return response.data
+  },
+  (error) => {
+    //处理网络错误
+    let msg = ''
+    const status = error.response.status
+    switch (status) {
+      case 401:
+        msg = 'token过期'
+        break
+      case 403:
+        msg = '无权访问'
+        break
+      case 404:
+        msg = '请求地址错误'
+        break
+      case 500:
+        msg = '服务器出现问题'
+        break
+      default:
+        msg = '无网络'
+    }
+    ElMessage({
+      type: 'error',
+      message: msg,
+    })
+    return Promise.reject(error)
+  },
+)
+// 对外暴露
+export default request
