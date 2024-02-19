8fc9b1a172dab6c4cf92cb074e6a3812fb43753c
123456hawkins <2398430768@qq.com>
2024-02-19 14:48:11 +0800

    feat: 新增mock插件

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,20 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+// 测试假接口
+axios({
+  url: '/api/user/login',
+  method: 'post',
+  data: {
+    username: 'admin',
+    password: '11111',
+  },
+}).then((res) => {
+  console.log(res)
+})
+</script>
 <style scoped lang="scss">
 .container {
   color: $color;
--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), //路径相对别名，用@代替src
-    },
-  },
-  // 配置全局变量
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variables.scss";',
+import { viteMockServe } from 'vite-plugin-mock'
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), //路径相对别名，用@代替src
       },
     },
-  },
+    // 配置全局变量
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variables.scss";',
+        },
+      },
+    },
+  }
 })
