9f870c03fe1781445f1dc91f0bb9a11445ddd67b
123456hawkins <2398430768@qq.com>
2024-02-19 11:46:11 +0800

    feat: 新增全局矢量图标

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>我的vite+vue项目</div>
+  <svg-icon name="phone" fill="red"></svg-icon>
+  <svg-icon name="back" fill="green"></svg-icon>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-var a = ref('1')
+let a = ref('1')
 console.log(a)
 </script>
 <style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'virtual:svg-icons-register'
 const app = createApp(App)
+// 注册全局组件
+import globalComponent from './components/index'
+app.use(globalComponent)
 app.use(ElementPlus)
 app.mount('#app')
